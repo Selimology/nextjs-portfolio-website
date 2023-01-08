b5b4b3aec933ecf83b0c2698670c0b25c7995e46
Selimology <kertekin2018@gmail.com>
2023-01-08 10:15:07 +0100

    chore: fix styling & add links

--- src/constants.ts
+++ src/constants.ts
@@ -3,3 +3,6 @@ export const GITHUB_PROFILE = 'https://github.com/Selimology';
 export const TWITTER_PROFILE = 'https://twitter.com/selimologycode';
 export const YOUTUBE_PROFILE =
   'https://www.youtube.com/channel/UC4H4uW80i1kLtaBg8J403Gw';
+
+export const RESUME_LINK =
+  'https://drive.google.com/file/d/1efimx_N4jOT688L52qYofrr92HsBK-wP/view?usp=sharing';
--- src/theme/index.ts
+++ src/theme/index.ts
@@ -1,9 +1,30 @@
-import { extendTheme, theme as base } from '@chakra-ui/react';
+import {
+  extendTheme,
+  StyleFunctionProps,
+  theme as base,
+} from '@chakra-ui/react';
 
 const theme = extendTheme({
+  components: {
+    Heading: {
+      baseStyle: (props: StyleFunctionProps) => ({
+        color: props.colorMode === 'dark' ? 'gray.100' : 'gray.700',
+      }),
+    },
+    Text: {
+      baseStyle: (props: StyleFunctionProps) => ({
+        color: props.colorMode === 'dark' ? 'gray.100' : 'gray.700',
+      }),
+    },
+  },
+  colors: {
+    linkedin: '#0077b5',
+    github: 'gray.700',
+    twitter: '#08a0e9',
+  },
   fonts: {
-    heading: `Roboto, ${base.fonts.heading}`,
-    body: `Roboto, ${base.fonts.body}`,
+    heading: `Inter, ${base.fonts.heading}`,
+    body: `Inter, ${base.fonts.body}`,
   },
 });
 
